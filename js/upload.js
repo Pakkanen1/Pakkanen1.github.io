05b4784f6301af6eb728c0786bce451134df75ef
Antti Pakkanen <apakkan15@labraverkko.turkuamk.fi>
2017-04-05 12:54:00 +0300

    Antin peeloilua

---
+++ js/upload.js
@@ -0,0 +1,50 @@
+var myApp = angular.module('myApp', []);
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
+
+        element.bind('change', function(){
+            scope.$apply(function(){
+                modelSetter(scope, element[0].files[0]);
+            });
+        });
+    }
+   };
+}]);
+
+// We can write our own fileUpload service to reuse it in the controller
+myApp.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl, name){
+         var fd = new FormData();
+         fd.append('file', file);
+         fd.append('name', name);
+         $http.post(uploadUrl, fd, {
+             transformRequest: angular.identity,
+             headers: {'Content-Type': undefined,'Process-Data': false}
+         })
+         .success(function(){
+            console.log("Success");
+         })
+         .error(function(){
+            console.log("Success");
+         });
+     }
+ }]);
+
+ myApp.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
+
+   $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+
+        var uploadUrl = "./upload.php";
+        var text = $scope.name;
+        fileUpload.uploadFileToUrl(file, uploadUrl, text);
+   };
+
+}]);
