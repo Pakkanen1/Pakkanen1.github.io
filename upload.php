87930e1f112c9736e2aa06cf427994a7699b9bfa
Pakkanen1 <bonetailur@gmail.com>
2017-04-18 21:01:07 +0300

    imageappender.js hakee etusivulle JSON-kannasta kuvat ja thumbnailit

--- upload.php
+++ upload.php
@@ -1,30 +1,45 @@
+
 <?php
-
-     $target_dir = "/test";
-     $name = $_POST['name'];
-     print_r($_FILES);
-     $target_file = $target_dir . basename($_FILES["file"]["name"]);
-
-     move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-
-     //write code for saving to database 
-     include_once "config.php";
-
-     // Create connection
-     $conn = new mysqli($servername, $username, $password, $dbname);
-     // Check connection
-     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-     }
-
-     $sql = "INSERT INTO MyData (name,filename) VALUES ('".$name."','".basename($_FILES["file"]["name"])."')";
-
-     if ($conn->query($sql) === TRUE) {
-         echo json_encode($_FILES["file"]); // new file uploaded
-     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-     }
-
-     $conn->close();
-
+$target_dir = "images/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 ?>
