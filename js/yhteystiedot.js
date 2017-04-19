038795f38aed1556454f6971b309c9073f5d2664
TRuonti <TRuonti>
2017-04-19 20:51:40 +0300

    Yhteystietojen muotoilua, HTML tiivistämistä ja jQuery-efektit.

--- js/yhteystiedot.js
+++ js/yhteystiedot.js
@@ -8,5 +8,37 @@ yTiedot.controller('InfoView', function ($scope) {
 		{name: "Arttu Seppä-Lassila", phone: "123-456-789", email: "arttu.lassila@whatever.com"},
 		{name: "Ilari Nikander", phone: "123-456-789", email: "ilari.nikander@whatever.com"}
 	];
+});
+
+$(document).ready(function() {
 	
+	$('#hlö').hide();
+	
+	$('#teemu').click(function () {
+		
+		$('#hlö').hide();
+		$('.teemu').slideDown(500);
+		
+	});
+	
+	$('#antti').click(function () {
+		
+		$('#hlö').hide();
+		$('.antti').slideDown(500);
+		
+	});
+	
+	$('#arttu').click(function () {
+		
+		$('#hlö').hide();
+		$('.arttu').slideDown(500);
+		
+	});
+	
+	$('#ilari').click(function () {
+		
+		$('#hlö').hide();
+		$('.ilari').slideDown(500);
+		
+	});
 });
