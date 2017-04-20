1e0b9a51233233255264a5e158c63339be7dc058
Pakkanen1 <bonetailur@gmail.com>
2017-04-20 12:03:53 +0300

    teemun tekemät yhteystiedot lisätty

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,1 +1,13 @@
-/// Tähän tulee kaikki sisäänkirjautumiseen littyvät toiminnot
+// Tähän tulee kaikki sisäänkirjautumiseen littyvät toiminnot
+$(document).ready(function() {
+	//Kirjautuminen piilossa aluksi.
+	$('#login-window').hide();
+
+	//Kirjautumisnappia painetaan
+	$('#login-button').click(function() {
+		//Kirjautumisikkuna joko vieritetään esiin tai pois, estetään stopilla ettei animaatiot kasaannu
+		$('#login-window').stop().slideToggle();
+
+	}); //click loppuu
+
+}); //ready loppuu
